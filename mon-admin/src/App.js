9b29b581ee2f84d3590f7c1830674f1f3734c580
reymouhid95 <thiernooury89@gmail.com>
2023-11-23 17:07:54 +0000

    Ajout de l'authentification

--- mon-admin/src/App.js
+++ mon-admin/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FormBook from "./components/BookForm";
+import SignUp from "./components/Inscription";
+import SignIn from "./components/Connexion";
 
 function App() {
   return (
     <div className="App">
       <FormBook />
+      <SignUp />
+      <SignIn />
     </div>
   );
 }
--- mon-admin/src/firebase-config.js
+++ mon-admin/src/firebase-config.js
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const provider = new FacebookAuthProvider();
 
-export { db, auth };
+export { db, auth, googleProvider, provider };
